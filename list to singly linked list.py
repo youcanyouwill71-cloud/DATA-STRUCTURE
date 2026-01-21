a513348d6c4c969b728486e2f41d01596f566f28
youcanyouwill71-cloud <youcanyouwill71@gmail.com>
2026-01-21 08:46:37 +0530

    Update list to singly linked list.py

--- list to singly linked list.py
+++ list to singly linked list.py
@@ -32,3 +32,29 @@ if __name__ == "__main__":
     head = create_linked_list(arr)
 
     print_linked_list(head)
+
+
+//method easy
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+if __name__ == "__main__":
+
+    arr = [2, 5, 8, 7]
+
+    y = Node(arr[0])      # head node
+    temp = y              # pointer to build the list
+
+    for i in range(1, len(arr)):
+        temp.next = Node(arr[i])
+        temp = temp.next
+
+    # print linked list
+    curr = y
+    while curr:
+        print(curr.data, end=" -> ")
+        curr = curr.next
+    print("None")
